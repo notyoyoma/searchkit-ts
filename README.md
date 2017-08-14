219de2af72e6edab0d9eb6e5fc773b888628e0b9
mnaselli <marty.naselli@gmail.com>
2017-08-14 15:48:19 -0400

    initial commit, not working b/c typescript

---
+++ README.md
@@ -0,0 +1,5 @@
+### Get started
+```
+yarn
+yarn start
+```
---
+++ src/App.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import './App.css';
+
+import { SearchkitManager,SearchkitProvider,
+  SearchBox, RefinementListFilter, Pagination,
+  HierarchicalMenuFilter, HitsStats, SortingSelector, NoHits,
+  ResetFilters, RangeFilter, NumericRefinementListFilter,
+  ViewSwitcherHits, ViewSwitcherToggle, DynamicRangeFilter,
+  InputFilter, GroupedSelectedFilters,
+  Layout, TopBar, LayoutBody, LayoutResults,
+  ActionBar, ActionBarRow, SideBar } from 'searchkit'
+import './index.css'
+
+
+
+const logo = require('./logo.svg');
+const host = "http://demo.searchkit.co/api/movies"
+const searchkit = new SearchkitManager(host)
+
+class App extends React.Component {
+  render() {
+    return (
+      <SearchkitProvider searchkit={searchkit}>
+        <Layout>
+          <TopBar>
+            <div className="my-logo">Searchkit Acme co</div>
+            <SearchBox autofocus={true} searchOnChange={true} prefixQueryFields={["actors^1","type^2","languages","title^10"]}/>
+          </TopBar>
+
+        <LayoutBody>
+
+          <SideBar>
+            <HierarchicalMenuFilter fields={["type.raw", "genres.raw"]} title="Categories" id="categories"/>
+            <DynamicRangeFilter field="metaScore" id="metascore" title="Metascore" rangeFormatter={(count)=> count + "*"}/>
+            <RangeFilter min={0} max={10} field="imdbRating" id="imdbRating" title="IMDB Rating" showHistogram={true}/>
+            <InputFilter id="writers" searchThrottleTime={500} title="Writers" placeholder="Search writers" searchOnChange={true} queryFields={["writers"]} />
+            <RefinementListFilter id="actors" title="Actors" field="actors.raw" size={10}/>
+            <RefinementListFilter translations={{"facets.view_more":"View more writers"}} id="writers" title="Writers" field="writers.raw" operator="OR" size={10}/>
+            <RefinementListFilter id="countries" title="Countries" field="countries.raw" operator="OR" size={10}/>
+            <NumericRefinementListFilter id="runtimeMinutes" title="Length" field="runtimeMinutes" options={[
+              {title:"All"},
+              {title:"up to 20", from:0, to:20},
+              {title:"21 to 60", from:21, to:60},
+              {title:"60 or more", from:61, to:1000}
+            ]}/>
+          </SideBar>
+          <LayoutResults>
+            <ActionBar>
+
+              <ActionBarRow>
+                <HitsStats translations={{
+                  "hitstats.results_found":"{hitCount} results found"
+                }}/>
+                <ViewSwitcherToggle/>
+                <SortingSelector options={[
+                  {label:"Relevance", field:"_score", order:"desc"},
+                  {label:"Latest Releases", field:"released", order:"desc"},
+                  {label:"Earliest Releases", field:"released", order:"asc"}
+                ]}/>
+              </ActionBarRow>
+
+              <ActionBarRow>
+                <GroupedSelectedFilters/>
+                <BooleanFilters/>
+                <ResetFilters/>
+              </ActionBarRow>
+
+            </ActionBar>
+            <ViewSwitcherHits
+                hitsPerPage={12} highlightFields={["title","plot"]}
+                sourceFilter={["plot", "title", "poster", "imdbId", "imdbRating", "year"]}
+                hitComponents={[
+                  {key:"grid", title:"Grid", itemComponent:MovieHitsGridItem, defaultOption:true},
+                  {key:"list", title:"List", itemComponent:MovieHitsListItem}
+                ]}
+                scrollTo="body"
+            />
+            <NoHits suggestionsField={"title"}/>
+            <Pagination showNumbers={true}/>
+          </LayoutResults>
+
+          </LayoutBody>
+        </Layout>
+      </SearchkitProvider>
+    );
+  }
+}
+
+export default App;
